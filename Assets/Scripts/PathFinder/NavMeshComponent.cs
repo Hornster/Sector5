29ecc2f67547a21a2267ab9e27c0b443066d1599
MGruchlik <s>
2020-10-16 16:07:16 +0200

    Test NavMeshAgent

---
+++ Assets/Scripts/PathFinder/NavMeshComponent.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class NavMeshComponent : MonoBehaviour
+{
+    NavMeshAgent navMeshAgent;
+    [SerializeField]
+    GameObject target;
+    Vector3 targetPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        SetDestination(target.transform.position);
+    }
+
+    public void SetDestination(Vector3 vector3)
+    {
+        targetPos = vector3;
+    }
+
+    public void SetDestination(GameObject gameObject)
+    {
+        navMeshAgent.SetDestination(gameObject.transform.position);
+    }
+
+    public void Move()
+    {
+        //navMeshAgent.SetDestination(vector3);
+        NavMeshPath navMeshPath = new NavMeshPath();
+        navMeshAgent.CalculatePath(targetPos, navMeshPath);
+        
+        if (navMeshPath.status.Equals(NavMeshPathStatus.PathComplete))
+        {
+            navMeshAgent.isStopped = false;
+            navMeshAgent.SetPath(navMeshPath);
+        }
+        else
+        {
+            navMeshAgent.isStopped = true;
+        }
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+}
