0b939c989fe9d4efb7ca2e3b1bc78318f304d5ae
Karol Kozuch <karol199714@gmail.com>
2020-11-03 18:29:32 +0100

    Clearing input whenever command is issued

---
+++ Assets/Scripts/UI/Console/InputFieldController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMPro;
+using UnityEngine;
+
+namespace Assets.Scripts.UI.Console
+{
+    public class InputFieldController : MonoBehaviour
+    {
+        [Tooltip("Ref to the field that is used for inputting commands into the console.")]
+        [SerializeField] 
+        private TMP_InputField _inputField;
+
+
+        public void ResetInput()
+        {
+            _inputField.text = "";
+        }
+    }
+}
